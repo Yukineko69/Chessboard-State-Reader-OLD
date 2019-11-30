6f51c1cdf83a3e89a4395f364bae6d0635a49a86
Tuan <tegieng9a4@gmail.com>
2019-11-30 14:08:04 +0700

    commit

--- adaptive_threshold_test.py
+++ adaptive_threshold_test.py
@@ -16,7 +16,7 @@ def inverte(img):
     img = (255-img)
     return img
 
-img = cv2.imread('./board/12.jpg')
+img = cv2.imread('./board/13.jpg')
 img = imutils.resize(img, width=500)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #cv2.imshow('img', img)
@@ -70,22 +70,20 @@ for c in range(len(contours)):
 # Draw contour
 cv2.drawContours(imgContours, [largest], -1, (0,255,0), 2)
 cv2.imshow('ct', imgContours)
-# =============================================================================
-# # Epsilon parameter needed to fit contour to polygon
-# epsilon = 0.1 * Lperimeter
-# # Approximates a polygon from chessboard edge
-# chessboardEdge = cv2.approxPolyDP(largest, epsilon, True)
-# 
-# # Create new all black image
-# mask = np.zeros((img.shape[0], img.shape[1]), 'uint8')*125
-# # Copy the chessboard edges as a filled white polygon size of chessboard edge
-# cv2.fillConvexPoly(mask, chessboardEdge, 255, 1)
-# # Assign all pixels that are white (i.e the polygon, i.e. the chessboard)
-# extracted = np.zeros_like(img)
-# extracted[mask == 255] = img[mask == 255]
-# # remove strip around edge
-# extracted[np.where((extracted == [125, 125, 125]).all(axis=2))] = [0, 0, 20]
-# 
-# cv2.imshow('ct', imgContours)
-# cv2.imshow('ex', extracted)
-# =============================================================================
+# Epsilon parameter needed to fit contour to polygon
+epsilon = 0.1 * Lperimeter
+# Approximates a polygon from chessboard edge
+chessboardEdge = cv2.approxPolyDP(largest, epsilon, True)
+
+# Create new all black image
+mask = np.zeros((img.shape[0], img.shape[1]), 'uint8')*125
+# Copy the chessboard edges as a filled white polygon size of chessboard edge
+cv2.fillConvexPoly(mask, chessboardEdge, 255, 1)
+# Assign all pixels that are white (i.e the polygon, i.e. the chessboard)
+extracted = np.zeros_like(img)
+extracted[mask == 255] = img[mask == 255]
+# remove strip around edge
+extracted[np.where((extracted == [125, 125, 125]).all(axis=2))] = [0, 0, 20]
+
+cv2.imshow('ct', imgContours)
+cv2.imshow('ex', extracted)
