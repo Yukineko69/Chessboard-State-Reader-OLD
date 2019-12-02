dd33dd80047ae6344124cdaa850b9b7ce35d6db0
Tuan <tegieng9a4@gmail.com>
2019-12-02 22:48:06 +0700

    commit

--- adaptive_threshold_test.py
+++ adaptive_threshold_test.py
@@ -42,28 +42,42 @@ def removeBackground(img):
     
     return extracted
 
-imgpath = './board/13.jpg'
+def getChessboardEdges(img):
+    canny = cv2.Canny(img, 10, 250)    
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    closed = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+    closed = cv2.erode(closed, kernel, iterations=1)
+    closed = cv2.dilate(closed, kernel, iterations=1)
+    cv2.imshow('closed', closed)
+    
+    cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    c = sorted(cnts, key = cv2.contourArea, reverse = True)[1]
+    peri = cv2.arcLength(c, True)
+    edgeApprox = cv2.approxPolyDP(c, 0.1 * peri, True)
+    
+    imgContours = img.copy()
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.drawContours(imgContours, [edgeApprox], -1, (0,255,0), 2)
+    mask = np.zeros_like(img_gray)
+    cv2.fillConvexPoly(mask, edgeApprox, 255, 1)
+    extracted = np.zeros_like(img)
+    extracted[mask == 255] = img[mask == 255]
+    
+    blur = cv2.medianBlur(extracted, 3)
+    canny = cv2.Canny(blur, 50, 250)
+    
+    return canny, extracted, edgeApprox
+    
+
+imgpath = './board/11.jpg'
 img = cv2.imread(imgpath)
 img = imutils.resize(img, width=400)
 
 extracted = removeBackground(img)
 cv2.imshow('ex', extracted)
 
+canny, ex2, edges = getChessboardEdges(extracted)
+cv2.imshow('canny', canny)
+cv2.imshow('ex2', ex2)
 
 
-
-#edged = cv2.Canny(extracted, 10, 250)
-#cv2.imshow('Canny', edged)
-#
-#kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-#closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-#closed = cv2.erode(closed, None, iterations=2)
-#closed = cv2.dilate(closed, None, iterations=1)
-#cv2.imshow("Closed", closed)
-#
-#cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
-#peri = cv2.arcLength(c, True)
-#approx = cv2.approxPolyDP(c, 0.1 * peri, True)
-#cv2.drawContours(extracted, [approx], -1, (0, 255, 0), 2)
-#cv2.imshow('ex', extracted)
