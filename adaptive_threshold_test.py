e37b793c28d7e1abcb443380689c9afc57d7223e
Tuan <tegieng9a4@gmail.com>
2019-12-03 11:53:06 +0700

    commit

--- adaptive_threshold_test.py
+++ adaptive_threshold_test.py
@@ -9,6 +9,7 @@ Created on Thu Nov 28 22:28:26 2019
 import cv2
 import numpy as np
 import imutils
+import math
 
 
 def removeBackground(img):
@@ -48,7 +49,7 @@ def getChessboardEdges(img):
     closed = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
     closed = cv2.erode(closed, kernel, iterations=1)
     closed = cv2.dilate(closed, kernel, iterations=1)
-    cv2.imshow('closed', closed)
+#    cv2.imshow('closed', closed)
     
     cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     c = sorted(cnts, key = cv2.contourArea, reverse = True)[1]
@@ -63,21 +64,88 @@ def getChessboardEdges(img):
     extracted = np.zeros_like(img)
     extracted[mask == 255] = img[mask == 255]
     
-    blur = cv2.medianBlur(extracted, 3)
+    return extracted, edgeApprox
+
+def checkLineOrientation(line):
+    [[x1, y1, x2, y2]] = line
+    dx = x2 - x1
+    dy = y2 - y1
+    if abs(dx) > abs(dy):
+        return "horizontal"
+    else:
+        return "vertical"
+
+def lineSegmentation(img):
+    blur = cv2.medianBlur(img, 3)
     canny = cv2.Canny(blur, 50, 250)
     
-    return canny, extracted, edgeApprox
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    canny = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+    canny = cv2.dilate(canny, None, iterations=1)
     
+    lines = cv2.HoughLinesP(canny, 1, np.pi/180, 150, minLineLength=200, maxLineGap=10)
+    for line in lines:
+        x1,y1,x2,y2 = line[0]
+        cv2.line(img, (x1,y1), (x2,y2), (0,255,0), 2)
+        
+    horizontal = []
+    vertical = []
+    for line in lines:
+        if checkLineOrientation(line) == 'horizontal':
+            horizontal.append(line)
+        else:
+            vertical.append(line)
+    
+    return img, horizontal, vertical
+    
+def findIntersect(h, v):
+    [[x1, y1, x2, y2]] = h
+    [[X1, Y1, X2, Y2]] = v
+    
+    x = ((x1*y2 - x2*y1) * (X1-X2) - (x1 - x2) * (X1*Y2 - X2*Y1)) / \
+        ((x1 - x2) * (Y1 - Y2) - (y1 - y2) * (X1 - X2))
+    y = ((x1*y2 - x2*y1) * (Y1-Y2) - (y1 - y2) * (X1*Y2 - X2*Y1)) / \
+        ((x1 - x2) * (Y1 - Y2) - (y1 - y2) * (X1 - X2))
+    x = int(x)
+    y = int(y)
+    return [x,y]
 
-imgpath = './board/11.jpg'
+def detectCorners(img, vertical, horizontal):
+    corners = []
+    for v in vertical:
+        for h in horizontal:
+            corner = findIntersect(h, v)
+            corners.append(corner)
+    
+    dedupeCorners = []
+    for c in corners:
+        matchingFlag = False
+        for d in dedupeCorners:
+            if math.sqrt((d[0]-c[0])*(d[0]-c[0]) + (d[1]-c[1])*(d[1]-c[1])) < 20:
+                matchingFlag = True
+                break
+        if not matchingFlag:
+            dedupeCorners.append(c)
+    
+    for c in dedupeCorners:
+        cv2.circle(img, (c[0], c[1]), 10, (0,0,255))
+        
+    return img, dedupeCorners
+
+imgpath = './board/13.jpg'
 img = cv2.imread(imgpath)
-img = imutils.resize(img, width=400)
+img = imutils.resize(img, width=500)
+cv2.imshow('img', img)
 
 extracted = removeBackground(img)
-cv2.imshow('ex', extracted)
+cv2.imshow('ex', extracted)    
 
-canny, ex2, edges = getChessboardEdges(extracted)
-cv2.imshow('canny', canny)
+ex2, edges = getChessboardEdges(extracted)
+
+ex2, horizontal, vertical = lineSegmentation(ex2)
+
 cv2.imshow('ex2', ex2)
 
-
+ex2, corners = detectCorners(ex2, vertical, horizontal)
+    
+cv2.imshow('ex2', ex2)
